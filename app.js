b7b793366d2ad2bc7942d03cdc10d8af8b8b5a69
Arifahanggraini05 <arifahanggraini05@gmail.com>
2025-01-05 17:36:08 +0000

    app.js

--- app.js
+++ app.js
@@ -73,4 +73,16 @@ window.onclick = function(event) {
     if (event.target == modal) {
         closeModal();
     }
-};
+}
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch((error) => {
+        console.error('Service Worker registration failed:', error);
+      });
+  });
+}
+
